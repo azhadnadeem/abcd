2998dee8f78df212d57accfc2d8dd445b101f9a9
Azhad Nadeem <74958830+azhadnadeem@users.noreply.github.com>
2021-10-18 03:53:30 -0700

    Add files via upload

---
+++ System.js
@@ -0,0 +1,17 @@
+class System{
+
+    constructor(){}
+
+    authenticate(bu=createSprite(500,330,30,30)){
+       // actualCode,enteredCode
+        textSize(50);
+        fill("black");
+        text(code,300,300)
+        if((bu=createSprite(500,330,30,30)) )
+       // (actualCode === enteredCode.toUpperCase
+            return true
+        else
+            return false
+    }
+
+}
